c6056095a3da08cbef8515c6bb47191a59fafd56
Petrik Gábor <admin@twipsi.com>
2022-10-27 12:22:28 +0200

    Update simple.php

--- middleware/simple.php
+++ middleware/simple.php
@@ -12,7 +12,7 @@ return [
   'csrf'           => \Twipsi\Components\Http\Middlewares\CsrfTokenVerification::class,
   'guest'          => \Twipsi\Components\Authentication\Middlewares\RedirectIfAuthenticated::class,
   'authenticated'  => \Twipsi\Components\Authentication\Middlewares\RedirectIfNotAuthenticated::class,
-  'verified'       => \Twipsi\Components\Authentication\Middlewares\RedirectIfEmailnotVerified::class,
+  'verified'       => \Twipsi\Components\Authentication\Middlewares\RedirectIfEmailNotVerified::class,
   'valid'          => \Twipsi\Components\Authentication\Middlewares\RedirectIfAccountInvalid::class,
   'signed'         => \Twipsi\Bridge\Middlewares\ValidateSignature::class,
 ];
